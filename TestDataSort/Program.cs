ddfa8129f6d8cf80deb3978342af9af52bdc53ec
ProducerBill <william.c.jones1981@gmail.com>
2021-03-28 20:57:44 -0400

    Posting Test Code and Data.
    Initial posting of the test code and data.

---
+++ TestDataSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDataSort
+{
+    class Program
+    {
+        static List<List<MachineData>> allData = new List<List<MachineData>>();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
